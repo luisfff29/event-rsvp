f532cbf044c6b3560e5053e10f96c4207f9131b3
luisfff29 <luisfff29@gmail.com>
2020-08-23 03:32:18 -0500

    make post request and get info

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const pug = require("pug");
 const port = 3000;
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.set("view engine", "pug");
 app.use(express.static("./public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.post("/reply", (req, res) => {
+  console.log(req.body);
+  res.send("Thank you for your response!");
 });
 
 app.listen(port, () => {
