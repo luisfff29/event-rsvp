2f6758a2ccb8074e5f91f0af7cb5020a453a277b
luisfff29 <luisfff29@gmail.com>
2020-08-24 11:33:05 -0500

    update guests field for numbers

--- app.js
+++ app.js
@@ -15,29 +15,32 @@ app.set("view engine", "pug");
 app.use(express.static("./public"));
 app.use(express.urlencoded({ extended: true }));
 
-const user = Rsvp;
-
 app.get("/", (req, res) => {
   res.render("index");
 });
 
 app.post("/reply", (req, res) => {
-  res.setHeader("Content-type", "text/html");
-  res.send(
-    `Thank you for your response! <br> <a href="/guests">Check list of guests</a>`
-  );
+  var guests = 0;
+  if (req.body.guests !== undefined) {
+    guests = req.body.guests;
+  }
 
   const user = new Rsvp({
     name: req.body.name,
     email: req.body.email,
-    attending: true ? req.body.attending == "yes" : false,
-    guests: Number(req.body.guests),
+    attending: true ? req.body.attending === "yes" : false,
+    guests: guests,
   });
 
   user.save((err, user) => {
     if (err) console.error(err);
     console.log(user);
   });
+
+  res.setHeader("Content-type", "text/html");
+  res.send(
+    `Thank you for your response! <br> <a href="/guests">Check list of guests</a>`
+  );
 });
 
 app.get("/guests", (req, res) => {
